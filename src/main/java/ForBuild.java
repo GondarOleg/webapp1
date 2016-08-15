3b50dd88d5c24c7f5245801dddacf85a6adc8644
Oleg Gondar <Oleg_Gondar@epam.com>
2016-08-15 16:48:30 +0300

    Initial commit

---
+++ src/main/java/ForBuild.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Oleg_Gondar on 8/15/2016.
+ */
+public class ForBuild {
+
+    public static boolean alwaysTrue(){
+        return true;
+    }
+
+}
---
+++ src/test/java/Tests.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+/**
+ * Created by Oleg_Gondar on 8/15/2016.
+ */
+public class Tests {
+
+    @Test
+    public void alwaysTrue(){
+        assertTrue(ForBuild.alwaysTrue());
+    }
+
+}
