4897251de3a969b974e95190bc5e70e1bd6d5e1a
Oleg Gondar <Oleg_Gondar@epam.com>
2016-08-15 18:13:26 +0300

    bad commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+/**
+ * Created by Oleg_Gondar on 8/15/2016.
+ */
+public class Test1 {
+
+    @Test
+    public void alwaysTrue(){
+        assertTrue(ForBuild.alwaysFalse());
+
+
+    }
+
+
+
+}
--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,23 +1,15 @@
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+
 import static org.junit.Assert.assertFalse;
+
 /**
  * Created by Oleg_Gondar on 8/15/2016.
  */
-public class Tests {
-
-    @Test
-    public void alwaysTrue(){
-        assertTrue(ForBuild.alwaysFalse());
-
-
-    }
-
+public class Test2 {
     @Test
     public void alwaysFalse(){
 
         assertFalse(ForBuild.alwaysTrue());
 
     }
-
 }
